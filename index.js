8de35aa3d79181a97368f04d436e1081a5bf9e52
martinharts <89169036+martinharts@users.noreply.github.com>
2021-08-31 22:24:07 -0400

    imported atm into index.js. created function for start of menu. created menu function with switch case for choices.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 "use strict";
+const prompt = require('prompt-sync')();
+const atm = require('./atm'); //import of atm to index
+//calling start function to start atm menu
+start();
+
+function start(){
+    let bank = "Navy Federal Credit Union";
+    console.log(`welcome to ${bank}!!`);
+    atm.pinVerify();
+}
+
+function menu(){
+    console.log("what would you like to do? /n 1: Balance /n 2: Withdraw 3: Deposit")
+    let user = prompt();
+    switch(user){
+        case "1":
+            atm.balance();
+            break;
+        case "2":
+            atm.withdrawing();
+            break;
+        case "3":
+            atm.deposit();
+    }
+}
+
