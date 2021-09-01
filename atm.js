9e18c3a54ce05ce857fd69f60a8d99afd1298e90
martinharts <89169036+martinharts@users.noreply.github.com>
2021-08-31 21:05:43 -0400

    started on creating functions for getting balance, verifying pin, and depositting money into account

--- atm.js
+++ atm.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const prompt = require('prompt-sync')();// prompt-synce implementation
+const account = require('./account'); //import from account
+
+function getBalance(){
+    console.log("Your account balance is" + " " + account.balance)
+}
+
+function verifyPin(){
+    let user = prompt("Enter Pin Number")
+    if(user === account.pin){
+        console.log("Correct pin!")
+    }
+    else if(user != account.pin){
+        console.log("Incorrect pin! Try again!")
+        verifyPin(user)
+    }
+}
+
+function deposit(){
+    
+}
